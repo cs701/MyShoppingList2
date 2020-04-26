a43249926abf59574317d83ccc192a515633d41c
sdighe-ctrl <55038011+sdighe-ctrl@users.noreply.github.com>
2020-04-26 15:50:48 -0400

    Add files via upload

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Router } from '@angular/router'; 
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  @Input() email = "";
+  @Input() password = "";
+  constructor(private router:Router) { }
+
+  ngOnInit(): void {
+  }
+
+  goToRegister(){    
+    this.router.navigate(['/registration']);  
+  } 
+
+  goToHome(){   
+    const reqObj = {"email":this.email, "password":this.password} ;
+    console.log("login req obj--->"+JSON.stringify(reqObj));
+    this.router.navigate(['/main']);  
+  } 
+
+}
