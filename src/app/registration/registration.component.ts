430505646681b59128145f2f78ed539820595dc7
sdighe-ctrl <55038011+sdighe-ctrl@users.noreply.github.com>
2020-04-27 19:53:09 -0400

    Add files via upload

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-registration',
@@ -12,8 +13,8 @@ export class RegistrationComponent implements OnInit {
   @Input() email = "";
   @Input() psw = "";
   @Input() psw_repeat = "";
-
-  constructor(private router:Router) { }
+  postId;
+  constructor(private router:Router,private http: HttpClient) { }
 
   ngOnInit(): void {
   }
@@ -23,9 +24,19 @@ export class RegistrationComponent implements OnInit {
   }
 
   goToRegister(){  
-    const registerObj = {"fname" : this.fname, "lname" : this.lname, "email": this.email, "psw": this.psw, "psw_repeat": this.psw_repeat,"action":"up"} 
-    console.log("register req obj--->"+JSON.stringify(registerObj)); 
-    this.router.navigate(['/main']);  
+   // const registerObj = {"fname" : this.fname, "lname" : this.lname, "email": this.email, "psw": this.psw, "psw_repeat": this.psw_repeat,"action":"up"} 
+    //console.log("register req obj--->"+JSON.stringify(registerObj)); 
+    //this.router.navigate(['/main']);  
+
+    const reqObj = {"email":this.email, "password":this.psw, "action":"up"} ;
+    
+    this.http.post<any>('http://localhost:1337/login', reqObj).subscribe(data => {
+            this.postId = data.uid;
+            //alert(this.postId);
+            localStorage.setItem('uid', this.postId);
+            alert("data from local storage---"+localStorage.getItem('uid'));
+            this.router.navigate(['/main']); 
+    })
   } 
 
 }
